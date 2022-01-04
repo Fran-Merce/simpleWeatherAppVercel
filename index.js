ff013b5ca53bd9facf662ac401fd0e70fa861937
Francisco Merce <franmerce1@gmail.com>
2022-01-03 21:24:10 -0300

    validations input and mediaQueries

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ const humidityElem = document.querySelector("#humidity");
 const windElem = document.querySelector("#wind");
 window.addEventListener("load", () => {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      lat = position.coords.latitude;
-      lon = position.coords.longitude;
-      const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=es&units=metric&appid=4e1567a23c7ba9bb05bb3eb7d19d4c6f`;
-      weatherFetch(URL);
-    },
-    (error) => {
-      if (error.code == error.PERMISSION_DENIED){
-        const URL = `https://api.openweathermap.org/data/2.5/weather?q=argentina&lang=es&units=metric&appid=4e1567a23c7ba9bb05bb3eb7d19d4c6f`;
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        lat = position.coords.latitude;
+        lon = position.coords.longitude;
+        const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=es&units=metric&appid=4e1567a23c7ba9bb05bb3eb7d19d4c6f`;
         weatherFetch(URL);
-    }
-    });
+      },
+      (error) => {
+        if (error.code == error.PERMISSION_DENIED) {
+          const URL = `https://api.openweathermap.org/data/2.5/weather?q=argentina&lang=es&units=metric&appid=4e1567a23c7ba9bb05bb3eb7d19d4c6f`;
+          weatherFetch(URL);
+        }
+      }
+    );
   }
-  
- 
 });
 
 const weatherFetch = (URL) => {
@@ -28,7 +28,7 @@ const weatherFetch = (URL) => {
       return response.json();
     })
     .then((data) => {
-      getSvg(data)
+      getSvg(data);
       getCountry(data);
       getTemperature(data);
       getHumidityAndWind(data);
@@ -43,31 +43,37 @@ const weatherFetch = (URL) => {
           "translateY(0px)";
         document.querySelector("#widgetError").style.visibility = "visible";
         loader.classList.toggle("loaderHidden");
+
         setTimeout(() => {
-          document.querySelector("#widgetError").style.transform ="translateY(-350%)";
+          document.querySelector("#widgetError").style.transform =
+            "translateY(-350%)";
           document.querySelector("#widgetError").style.visibility = "hidden";
-        }, 4000);
+        }, 50000);
       }
     });
 };
 
 const inputSearch = document.querySelector("input");
 inputSearch.addEventListener("keyup", (e) => {
-  if (e.key === "Enter") {
-    let city = inputSearch.value.replace(/" "/i, "");
+  const errorAlert = document.querySelector("#errorLenght");
+  let city = inputSearch.value.replace(/\s+/g, "");
+  if (city.length >= 3 && e.key === "Enter") {
     const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=es&units=metric&appid=4e1567a23c7ba9bb05bb3eb7d19d4c6f`;
     weatherFetch(URL);
     loader.classList.toggle("loaderHidden");
-    
+    errorAlert.classList.add("hidden");
+  } else if (city.length <= 2 && e.key === "Enter") {
+    errorAlert.textContent = "ingrese al menos 3 caracteres";
+    errorAlert.classList.remove("hidden");
   }
 });
 
 const getTemperature = (data) => {
   const descriptionDay = document.querySelector("#descriptionDay");
   const getTemp = document.querySelector("#tempDay");
+  const { description } = data.weather[0];
   let { temp } = data.main;
   temp = Math.round(temp);
-  const { description } = data.weather[0];
   getTemp.textContent = `Temperatura ${temp} °C`;
   descriptionDay.textContent = description;
 };
@@ -127,4 +133,3 @@ const getSvg = (data) => {
       body.style.backgroundImage = "url(assets/clear.jpg)";
   }
 };
-
